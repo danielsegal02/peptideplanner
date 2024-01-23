22864eaaf6c0d7352b8c3099484e547b1b538bd4
jubsaigh <jubrialsaigh@gmail.com>
2024-01-23 16:37:30 -0500

    progress on the MainWindow

--- MainWindow.py
+++ MainWindow.py
@@ -18,33 +18,46 @@ app.minsize(800, 600)
 top_label = tk.Label(app, text="Welcome to the Peptide Planner!")
 top_label.pack(pady=10)  # Adding some padding for better spacing
 
-# Frame to hold the first row of combo boxes
-combo_frame_one = tk.Frame(app)
-combo_frame_one.pack(pady=10)  # Adding some padding for better spacing
 
-# Create and pack the top 5 combo boxes
-for i in range(5):
-    # combo = ttk.Combobox(combo_frame_one, values=[f"Option {j+1}" for j in range(5)])  # past way to populate combobox
-    combo = ttk.Combobox(combo_frame_one, values=list(amino_acid_dict.keys()))
-    combo.pack(side=tk.LEFT, padx=5)  # Pack each combo box side by side with some padding
+##############################################################################################################################
+# Select Box code, may use later
 
-# Frame to hold the second row of combo boxes
-combo_frame_two = tk.Frame(app)
-combo_frame_two.pack(pady=10)  # Adding some padding for better spacing
+# # Frame to hold the first row of combo boxes
+# combo_frame_one = tk.Frame(app)
+# combo_frame_one.pack(pady=10)  # Adding some padding for better spacing
 
-# Create and pack the bottom 5 combo boxes
-for i in range(5):
-    combo = ttk.Combobox(combo_frame_two, values=[f"Option {j+1}" for j in range(5)])
-    combo.pack(side=tk.LEFT, padx=5)  # Pack each combo box side by side with some padding
+# # Create and pack the top 5 combo boxes
+# for i in range(5):
+#     # combo = ttk.Combobox(combo_frame_one, values=[f"Option {j+1}" for j in range(5)])  # past way to populate combobox
+#     combo = ttk.Combobox(combo_frame_one, values=list(amino_acid_dict.keys()))
+#     combo.pack(side=tk.LEFT, padx=5)  # Pack each combo box side by side with some padding
+
+# # Frame to hold the second row of combo boxes
+# combo_frame_two = tk.Frame(app)
+# combo_frame_two.pack(pady=10)  # Adding some padding for better spacing
+
+# # Create and pack the bottom 5 combo boxes
+# for i in range(5):
+#     combo = ttk.Combobox(combo_frame_two, values=[f"Option {j+1}" for j in range(5)])
+#     combo.pack(side=tk.LEFT, padx=5)  # Pack each combo box side by side with some padding
+##############################################################################################################################
+
+
+
+# Create an Entry widget
+entry = tk.Entry(app, width=50, font=('Arial 12'))
+entry.pack(padx=10, pady=10)  # restrict what user's can input later
 
 # Create and pack the button
-button = tk.Button(app, text="Click Me", command=on_button_click)
+button = tk.Button(app, text="Generate Peptide", command=on_button_click)
 button.pack(pady=10)  # Adding some padding for better spacing
 
 # Create and pack the label
 button_label = tk.Label(app, text="Press the button...")
 button_label.pack(pady=10)  # Adding some padding for better spacing
 
+
+# Image generation
 # Load the image (use an appropriate file path)
 image_path = "peptide_test_image.png"  # Replace with your image file path
 photo = PhotoImage(file=image_path)
@@ -68,6 +81,9 @@ label1.pack(pady=5, fill="x")
 # Create and pack the second text label
 label2 = tk.Label(label_frame, text="Net Charge:   ", font=("Arial", 14), anchor="w")
 label2.pack(pady=5, fill="x")
+
+
+
 
 # Start the application
 app.mainloop()
