de77772fb6ceb4243ac7cb8d9fa3a6c02c0247ec
NCZ021 <122950585+NCZ021@users.noreply.github.com>
2024-03-28 11:08:28 -0400

    Updated calc

--- Calculations.py
+++ Calculations.py
@@ -1,19 +1,46 @@
-from pyteomics import mass,electrochem
+import pandas as pd
 
-def format_peptide_sequence(input_string):
-    return input_string.replace('-', '')
+def calculate_mass(peptide_string):
+    # In case the input box is empty
+    if peptide_string == "":
+        return 0
 
-def calculate_mass_and_charge(peptide):
-    # Calculate the mass
-    peptide_mass = mass.calculate_mass(sequence=peptide)
+    # Load the CSV file
+    df = pd.read_csv("AminoAcidTable.csv")
     
-    # Calculate the charge.
-    peptide_charge = electrochem.charge(peptide, 7)
+    # Map each code to its Residue Mass
+    code_to_mass = df.set_index('Code')['Residue Mass'].to_dict()
+    
+    # Sum the Residue Mass for each code in the string (and account for the extra missing water mass)
+    total_mass = sum([code_to_mass[code] for code in peptide_string if code in code_to_mass]) + 18.01528
+    
+    return total_mass
 
-    return peptide_mass, peptide_charge
+def calculate_charge(peptide_string):
+    # Load the CSV file
+    df = pd.read_csv("AminoAcidTable.csv")
+    
+    # Map each code to its Charge
+    code_to_charge = df.set_index('Code')['Charge'].to_dict()
+    
+    # Sum the Charge for each code in the string
+    total_charge = sum([code_to_charge[code] for code in peptide_string if code in code_to_charge]) + 1
+    
+    return total_charge
 
+def calculate_reagent_mass(mol_equiv, reagent_MW, pepscale, resin_p_used):
+    #what do we do if the input boxes are empty? we should account for that.
+    resin_percent = resin_p_used * 0.01
+    return (mol_equiv*reagent_MW*pepscale*resin_percent)
 
-# DOES NOT WORK FOR AMINO ACIDS LIKE (am, ac, etc...)
+def calculate_reagent_vol(mol_equiv, reagent_MW, reagent_density, pepscale, resin_p_used, is_dry):
+    rg = 1
+    if is_dry == False:
+        rg = reagent_density** -1 #maybe set density to 1 and/or make it unmodifyable wwhen dry is true
+    #if dry is clicked reagent density will be 1 and if not it will be modifhable, so ahve that modified.
+    resin_percent = resin_p_used * 0.01
+    return (mol_equiv*reagent_MW*rg*pepscale*resin_percent) # might need to round up or down...
 
-
-# example_peptide_sequence_input = "ACDEFGHIK"
+def calculate_solvent_vol(volFactor, pepscale, resin_p_used):
+    resin_percent = resin_p_used * 0.01
+    return (10*volFactor*pepscale*resin_percent) 
